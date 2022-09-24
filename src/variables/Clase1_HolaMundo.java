43fa00953abb1e072f6afdad2153374fbd6ed547
ESTIVEN <cesar-2007-mesa@hotmail.com>
2022-09-24 01:14:05 -0500

    Primer commit de creación de proyecto

---
+++ src/variables/Clase1_HolaMundo.java
@@ -0,0 +1,12 @@
+package variables;
+
+public class Clase1_HolaMundo {
+
+	public static void main(String[] args) {
+		String saludar = "Hola";
+		
+		System.out.println(saludar.toUpperCase());
+		
+	}
+
+}
